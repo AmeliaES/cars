9d9ae2e3349f13a3b8b2db37615709e2d28fb875
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2024-03-30 17:55:09 +0000

    tried another package but i think it's out of date as code just hung

--- scrapeAT.py
+++ scrapeAT.py
@@ -1,13 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
+from autotrader_scraper import get_cars, save_csv, save_json
+results = get_cars(
+        make = "Audi",
+        model = "A5",
+        postcode = "SW1A 0AA",
+        radius = 1500,
+        min_year = 2005,
+        max_year = 2020,
+        include_writeoff = "include",
+        max_attempts_per_page = 5,
+        verbose = False
+    )
 
-URL = "https://www.autotrader.co.uk/car-search?make=Citroen&model=Berlingo&postcode=E17BT"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-
-
-with open("test.html", "w") as output:
-    output.write(str(soup))
-
-
+print(results)
